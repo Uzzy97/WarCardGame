0790745a09a5f0dc9d1923f97c0822ac90c4b223
uzzy97 <G00345816@gmit.ie>
2018-01-01 01:32:39 +0000

    Add project files.

---
+++ NewProject/Main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// Main
+
+// Variables
+int playerNumber = 0;
+int rounds = 13;
+char exit, save;
+void main() {
+
+	
+
+	// Input of amount of players
+	do {
+		printf("Please Enter The Amount Of Players (Between 2 & 10)\n");
+		scanf("%d", &playerNumber);
+	} while (playerNumber < 1 || playerNumber > 14);
+
+	// Rounds Track
+	for (int roundNumber = 1; roundNumber <= rounds; ) {
+		printf("Would you like to exit your current round ?");
+		scanf("%c", &exit);
+		if (exit = 'y' || 'Y')
+			gamesave();
+		else {
+			roundNumber++;
+		}
+	}	
+}
+// Getting user to save the game
+void gamesave() {
+	printf("Would you like to save the game ?");
+	scanf("%c", &save);
+}
