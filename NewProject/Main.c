cde1979b33ba41689cf888288d7b7f855e381c28
uzzy97 <G00345816@gmit.ie>
2018-01-07 20:22:51 +0000

    Final changes made to project, small bits added and commented.

--- NewProject/Main.c
+++ NewProject/Main.c
@@ -1,8 +1,15 @@
+/*
+Procedural Programming Project: WAR
+Student Name: Usman Sattar
+Student ID: G00345816
+*/
+
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
 #include <time.h> 
 
-//Struct Arrays
+// Struct Arrays
 typedef struct Player
 {
 	char playerName[50];
@@ -11,17 +18,18 @@ typedef struct Player
 }Player;
 
 // Declaring Methods
-void gameSave();
-int randomNum(int n);
+void saveGame();
+int randomNumber(int num);
 void cardDraw();
 
 // Declaring Global Variables
-char *suits[4] = { "spades","clubs","diamonds","hearts" };
+char *suits[4] = { "Spades","Clubs","Diamonds","Hearts" };
 int ranks[13] = { 2,3,4,5,6,7,8,9,10,11,12,13,14 };
 
-//Constants
+// Constants
 #define ROUNDS 13
 
+// Starting Of Main Method
 void main()
 {
 	// Declaring Variables
@@ -31,18 +39,19 @@ void main()
 	// Introduction To Game - Heading
 	printf("WELCOME TO THE ULTIMATE CARD GAME...WAR !\nARE YOU READY TO TAKE ON YOUR OPPONENTS...\n\n");
 
-	// A Do - While Loop, Getting Number Of Players
+	// A Do - While Loop, Getting Number Of Players Involved In The Game
 	do {
 		printf("Please Enter The Amount Of Players (Between 2 & 10): ");
-		scanf_s("%d", &playerNumber);
-		//Taking In Consideration Of The Amount Of Maximum Players
+		// Asking User Above Question
+		scanf("%d", &playerNumber);
+		//Taking In Consideration Of The Amount Of Maximum Players, Which Is 10
 	} while (playerNumber < 1 || playerNumber > 14);
 
-	// Calling The Method To Prompt User To Enter Name
-	void enterPlayerName(int player, Player listOfPlayers[10]);
-	enterPlayerName(playerNumber, listOfPlayers);
+	// Calling The Method To Prompt User To Enter Name, Maximum Player Per Game Are 10
+	void playerName(int player, Player listOfPlayers[10]);
+	playerName(playerNumber, listOfPlayers);
 
-	// Starting the rounds using for loop
+	// Starting The Rounds Using For Loop
 	int roundNumber = 1;
 	//Rounds Go Up To And Including 13
 	for (roundNumber; roundNumber <= ROUNDS;)
@@ -62,24 +71,39 @@ void main()
 		scanf("%s", &exit);
 
 		// If 'Y'||'y' Is Chosen, Then Game Is Saved And Exited
-		if (exit == 'y' || exit == 'Y') {
-			gameSave();
+		if (exit == 'y' || exit == 'Y')
+		{
+			saveGame();
 			roundNumber = 13;
 		}
-		else {
+		else
+		{
 			roundNumber++;
 			printf("\n\tNext round\n");
 		}
-	}
+	} // End Of For Loop
+
 	// Prompting Users That Game Is Over
 	printf("\n---GAME OVER---");
 	// Prompting Users To Add Up Scores
 	printf("\n---ADD UP YOUR SCORES TO SEE WHO THE WINNERS ARE---");
+	printf("\n---PLAYER WITH MAXIMUM POINTS WIN THE GAME---");
+	// Scoreboard - So Player Knows Where They Are Standing
+	printf("\n\nSCOREBOARD:");
+	printf("\n2 - 10: are equal from 2 to 10 points");
+	printf("\nJ (Jack) = 11 points");
+	printf("\nQ (Queen) = 12 points");
+	printf("\nK (King) = 13 points");
+	printf("\nA (Ace) = 14 points");
+	// Congratulations To The Winner
+	printf("\n\n---CONGRATULATIONS TO THE WINNER OF THE GAME---");
+	
+	// Makes Program Runnable Till End
 	getch();
 } // End Of Main 
 
 // Method To Prompt User To Enter Name
-void enterPlayerName(int player, Player listOfPlayers[10])
+void playerName(int player, Player listOfPlayers[10])
 {
 	int i = 0;
 	for (i; i < player; i++)
@@ -87,29 +111,30 @@ void enterPlayerName(int player, Player listOfPlayers[10])
 		char name[50];
 		// Player & Player Number - Followed By First Name
 		printf("Player (%d) Please Enter Your First Name: ", i + 1);
+		// Asking Players To Enter Their First Name
 		scanf("%s", name);
 	}
-} // End Of enterPlayerName() Method
+} // End Of playerName() Method
 
 // Random Number Generator
-int randomNum(int num)
+int randomNumber(int num)
 {
 	return rand() % num;
-} // End Of randomNum() Method
+} // End Of randomNumber() Method
 
 // Method For Drawing Cards
 void cardDraw()
 {
 	int r, s;
 	// Random
-	r = randomNum(13);
-	s = randomNum(4);
+	r = randomNumber(13);
+	s = randomNumber(4);
 
 	printf("\t\n\t%d of %s\n\n", ranks[r], suits[s]);
 } // End Of cardDraw() Method
 
 // Method For Saving Game
-void gameSave()
+void saveGame()
 {
 	char save;
 	printf("\nWould You Like To Save And Exit The Game? (Y = Yes || N = No)");
@@ -126,118 +151,6 @@ void gameSave()
 	}
 	else
 	{
-		printf("You Have Exited The Game Without Saving !");
+		printf("You Have Now Exited The Game Without Saving !");
 	}
-} // End Of gameSave() Method
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-#include <stdio.h>
-// Main
-void GameSave();
-
-// Variables
-int playerNumber = 0;
-int rounds = 13;
-char exit, save;
-
-#define CARDS_IN_DECK 13
-void main() {
-
-
-
-// Input of amount of players
-do {
-printf("Please Enter The Amount Of Players (Between 2 & 10)\n");
-scanf("%d", &playerNumber);
-} while (playerNumber < 1 || playerNumber > 14);
-
-// Rounds Track
-for (int roundNumber = 1; roundNumber <= rounds; ) {
-printf("Would you like to exit your current round ? (Y = Yes || N = No) \n");
-scanf("%s", &exit);
-if (exit = 'y' || 'Y')
-GameSave();
-else {
-roundNumber++;
-}
-}
-}
-// Getting user to save the game
-void GameSave() {
-char save;
-printf("Would you like to save the game ?\n");
-scanf("%s", &save);
-
-
-}
-
-typedef struct Card {
-char name[5];
-int value;
-} Card;
-
-void createNewDeck(Card newDeck[4][CARDS_IN_DECK])
-{
-
-
-Card cards[4][CARDS_IN_DECK] = {
-{
-{ "2", 2 },{ "3", 3 },{ "4", 4 },{ "5", 5 },{ "6", 6 },{ "7", 7 },{ "8", 8 },{ "9", 9 },
-{ "10", 10 },{ "J", 11 },{ "Q", 12 },{ "K", 13 },{ "A", 14 }
-},
-{
-{ "2", 2 },{ "3", 3 },{ "4", 4 },{ "5", 5 },{ "6", 6 },{ "7", 7 },{ "8", 8 },{ "9", 9 },
-{ "10", 10 },{ "J", 11 },{ "Q", 12 },{ "K", 13 },{ "A", 14 }
-},
-{
-{ "2", 2 },{ "3", 3 },{ "4", 4 },{ "5", 5 },{ "6", 6 },{ "7", 7 },{ "8", 8 },{ "9", 9 },
-{ "10", 10 },{ "J", 11 },{ "Q", 12 },{ "K", 13 },{ "A", 14 }
-},
-{
-{ "2", 2 },{ "3", 3 },{ "4", 4 },{ "5", 5 },{ "6", 6 },{ "7", 7 },{ "8", 8 },{ "9", 9 },
-{ "10", 10 },{ "J", 11 },{ "Q", 12 },{ "K", 13 },{ "A", 14 }
-}
-};
-}
-//void createNewDeck(Card newDeck[4][CARDS_IN_DECK]);
-
-
-
-
-*/
-
-
-
-/*char cards[4][13] = {
-
-{ '0','1','2','3','4','5','6','7','8','9','10','11','12', },
-{ '0','1','2','3','4','5','6','7','8','9','10','11','12', },
-{ '0','1','2','3','4','5','6','7','8','9','10','11','12', },
-{ '0','1','2','3','4','5','6','7','8','9','10','11','12', },
-
-
-
-};*/
-
-
-
-
-
-
-
-
+} // End Of saveGame() Method
